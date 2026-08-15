888902a73ce3c4ab3063d48004794561364c58e0
Qlovix <qlovix.co@gmail.com>
2026-08-15 12:19:32 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,9 @@ import Footer from "@/components/layout/Footer";
 import Loader from "@/components/layout/Loader";
 import Hero from "@/components/sections/Hero";
 import Philosophy from "@/components/sections/Philosophy";
-import Problem from "@/components/sections/Problem";
 import Capabilities from "@/components/sections/Capabilities";
-import Work from "@/components/sections/Work";
 import Process from "@/components/sections/Process";
-import Why from "@/components/sections/Why";
 import Technology from "@/components/sections/Technology";
-import Trust from "@/components/sections/Trust";
 import FinalCta from "@/components/sections/FinalCta";
 
 export default function Home() {
@@ -20,13 +16,9 @@ export default function Home() {
       <main>
         <Hero />
         <Philosophy />
-        <Problem />
         <Capabilities />
-        <Work />
         <Process />
-        <Why />
         <Technology />
-        <Trust />
         <FinalCta />
       </main>
       <Footer />
