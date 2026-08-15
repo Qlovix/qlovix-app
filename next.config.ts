ce057379c17314d86f0a76e23c764e646cd48385
Qlovix <qlovix.co@gmail.com>
2026-08-15 12:07:05 +0530

    Clear Next.js configuration settings
    Removed previous configuration settings for Next.js.

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,6 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
-  basePath: "/qlovix-app",
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
+
